7559035476500462b9827e83e4afeb17b458ed1c
Dominic Black <dpblack96@gmail.com>
2022-06-30 15:55:34 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const text = document.getElementById('text')
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    text.innerHTML = 'This is really cool!'
+  });
